50ffed832085e5e905dfa873d82353838e4efdb4
andi.novid <andi.novid@gmail.com>
2019-11-05 06:44:03 +0700

    upload file

---
+++ application/logs/log-2019-11-05.php
@@ -0,0 +1,114 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-11-05 00:32:05 --> Config Class Initialized
+INFO - 2019-11-05 00:32:05 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:32:05 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:32:06 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:32:06 --> URI Class Initialized
+DEBUG - 2019-11-05 00:32:06 --> No URI present. Default controller set.
+INFO - 2019-11-05 00:32:06 --> Router Class Initialized
+INFO - 2019-11-05 00:32:06 --> Output Class Initialized
+INFO - 2019-11-05 00:32:06 --> Security Class Initialized
+DEBUG - 2019-11-05 00:32:06 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:32:06 --> Input Class Initialized
+INFO - 2019-11-05 00:32:06 --> Language Class Initialized
+INFO - 2019-11-05 00:32:06 --> Language Class Initialized
+INFO - 2019-11-05 00:32:06 --> Config Class Initialized
+INFO - 2019-11-05 00:32:06 --> Loader Class Initialized
+INFO - 2019-11-05 00:32:06 --> Controller Class Initialized
+DEBUG - 2019-11-05 00:32:06 --> Welcome MX_Controller Initialized
+DEBUG - 2019-11-05 00:32:06 --> File loaded: E:\XAMPP\htdocs\senyumcinta\application\modules/Welcome/views/welcome_message.php
+INFO - 2019-11-05 00:32:06 --> Final output sent to browser
+DEBUG - 2019-11-05 00:32:06 --> Total execution time: 0.9554
+INFO - 2019-11-05 00:32:36 --> Config Class Initialized
+INFO - 2019-11-05 00:32:36 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:32:36 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:32:36 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:32:36 --> URI Class Initialized
+DEBUG - 2019-11-05 00:32:36 --> No URI present. Default controller set.
+INFO - 2019-11-05 00:32:36 --> Router Class Initialized
+INFO - 2019-11-05 00:32:37 --> Output Class Initialized
+INFO - 2019-11-05 00:32:37 --> Security Class Initialized
+DEBUG - 2019-11-05 00:32:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:32:37 --> Input Class Initialized
+INFO - 2019-11-05 00:32:37 --> Language Class Initialized
+INFO - 2019-11-05 00:32:37 --> Language Class Initialized
+INFO - 2019-11-05 00:32:37 --> Config Class Initialized
+INFO - 2019-11-05 00:32:37 --> Loader Class Initialized
+INFO - 2019-11-05 00:32:37 --> Controller Class Initialized
+DEBUG - 2019-11-05 00:32:37 --> Welcome MX_Controller Initialized
+DEBUG - 2019-11-05 00:32:37 --> File loaded: E:\XAMPP\htdocs\senyumcinta\application\modules/Welcome/views/welcome_message.php
+INFO - 2019-11-05 00:32:37 --> Final output sent to browser
+DEBUG - 2019-11-05 00:32:37 --> Total execution time: 0.3199
+INFO - 2019-11-05 00:33:39 --> Config Class Initialized
+INFO - 2019-11-05 00:33:39 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:33:39 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:33:39 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:33:39 --> URI Class Initialized
+INFO - 2019-11-05 00:33:39 --> Router Class Initialized
+INFO - 2019-11-05 00:33:39 --> Output Class Initialized
+INFO - 2019-11-05 00:33:39 --> Security Class Initialized
+DEBUG - 2019-11-05 00:33:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:33:39 --> Input Class Initialized
+INFO - 2019-11-05 00:33:39 --> Language Class Initialized
+ERROR - 2019-11-05 00:33:39 --> 404 Page Not Found: /index
+INFO - 2019-11-05 00:34:15 --> Config Class Initialized
+INFO - 2019-11-05 00:34:15 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:34:15 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:34:15 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:34:15 --> URI Class Initialized
+DEBUG - 2019-11-05 00:34:16 --> No URI present. Default controller set.
+INFO - 2019-11-05 00:34:16 --> Router Class Initialized
+INFO - 2019-11-05 00:34:16 --> Output Class Initialized
+INFO - 2019-11-05 00:34:16 --> Security Class Initialized
+DEBUG - 2019-11-05 00:34:16 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:34:16 --> Input Class Initialized
+INFO - 2019-11-05 00:34:16 --> Language Class Initialized
+INFO - 2019-11-05 00:34:16 --> Language Class Initialized
+INFO - 2019-11-05 00:34:16 --> Config Class Initialized
+INFO - 2019-11-05 00:34:16 --> Loader Class Initialized
+INFO - 2019-11-05 00:34:16 --> Controller Class Initialized
+DEBUG - 2019-11-05 00:34:16 --> Website MX_Controller Initialized
+DEBUG - 2019-11-05 00:34:16 --> File loaded: E:\XAMPP\htdocs\senyumcinta\application\modules/Website/views/welcome_message.php
+INFO - 2019-11-05 00:34:16 --> Final output sent to browser
+DEBUG - 2019-11-05 00:34:16 --> Total execution time: 0.5066
+INFO - 2019-11-05 00:34:28 --> Config Class Initialized
+INFO - 2019-11-05 00:34:28 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:34:28 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:34:28 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:34:28 --> URI Class Initialized
+DEBUG - 2019-11-05 00:34:28 --> No URI present. Default controller set.
+INFO - 2019-11-05 00:34:28 --> Router Class Initialized
+INFO - 2019-11-05 00:34:28 --> Output Class Initialized
+INFO - 2019-11-05 00:34:28 --> Security Class Initialized
+DEBUG - 2019-11-05 00:34:28 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:34:28 --> Input Class Initialized
+INFO - 2019-11-05 00:34:28 --> Language Class Initialized
+INFO - 2019-11-05 00:34:28 --> Language Class Initialized
+INFO - 2019-11-05 00:34:28 --> Config Class Initialized
+INFO - 2019-11-05 00:34:28 --> Loader Class Initialized
+INFO - 2019-11-05 00:34:28 --> Controller Class Initialized
+DEBUG - 2019-11-05 00:34:28 --> Website MX_Controller Initialized
+DEBUG - 2019-11-05 00:34:28 --> File loaded: E:\XAMPP\htdocs\senyumcinta\application\modules/Website/views/welcome_message.php
+INFO - 2019-11-05 00:34:28 --> Final output sent to browser
+DEBUG - 2019-11-05 00:34:28 --> Total execution time: 0.2693
+INFO - 2019-11-05 00:35:45 --> Config Class Initialized
+INFO - 2019-11-05 00:35:45 --> Hooks Class Initialized
+DEBUG - 2019-11-05 00:35:45 --> UTF-8 Support Enabled
+INFO - 2019-11-05 00:35:45 --> Utf8 Class Initialized
+INFO - 2019-11-05 00:35:45 --> URI Class Initialized
+DEBUG - 2019-11-05 00:35:45 --> No URI present. Default controller set.
+INFO - 2019-11-05 00:35:45 --> Router Class Initialized
+INFO - 2019-11-05 00:35:45 --> Output Class Initialized
+INFO - 2019-11-05 00:35:45 --> Security Class Initialized
+DEBUG - 2019-11-05 00:35:45 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-11-05 00:35:45 --> Input Class Initialized
+INFO - 2019-11-05 00:35:45 --> Language Class Initialized
+INFO - 2019-11-05 00:35:45 --> Language Class Initialized
+INFO - 2019-11-05 00:35:45 --> Config Class Initialized
+INFO - 2019-11-05 00:35:45 --> Loader Class Initialized
+INFO - 2019-11-05 00:35:45 --> Controller Class Initialized
+DEBUG - 2019-11-05 00:35:45 --> Main MX_Controller Initialized
+DEBUG - 2019-11-05 00:35:45 --> File loaded: E:\XAMPP\htdocs\senyumcinta\application\modules/Main/views/welcome_message.php
+INFO - 2019-11-05 00:35:45 --> Final output sent to browser
+DEBUG - 2019-11-05 00:35:45 --> Total execution time: 0.3321
